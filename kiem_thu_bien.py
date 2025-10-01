3ecec8b89e27169e0c248f3aa85805fbae440b2c
NgoTung1 <23021712@vnu.edu.vn>
2025-10-01 20:58:47 +0700

    init

--- kiem_thu_bien.py
+++ kiem_thu_bien.py
@@ -16,20 +16,3 @@ from tien_ship import tienShip
 def test_tien_ship_bien(a, b, expected):
     assert tienShip(a, b) == expected
 
-@pytest.mark.parametrize("a, b, expected", [
-    (-1, "no", "Đầu vào không hợp lệ"),
-    (3, "no", 12000),
-    (8, "no", 20000),
-    (11, "no", "Quá quãng đường quy định"),
-    (1, "normal", 0),
-    (3, "normal", 7000),
-    (8, "normal", 15000),
-    (15,"normal","Quá quãng đường quy định"),
-    (3, "freeship", 0),
-    (5, "freeship", 0),
-    (9, "freeship", 0),
-    (20,"freeship","Quá quãng đường quy định")
-])
-
-def test_tien_ship_bang_quyet_dinh(a, b, expected):
-    assert tienShip(a, b) == expected
